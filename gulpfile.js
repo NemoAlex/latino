35b266989341da2673aa7fefeb77dea81d3bdc21
Nemo <zhhjchina@gmail.com>
2016-10-07 20:05:37 +0800

    Remove .html extname

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var gulp = require('gulp')
   , watch = require('gulp-watch')
   , affected = require('gulp-jade-find-affected')
   , gulpFilter = require('gulp-filter')
+  , path = require('path')
 
   , watchify = require('watchify')
   , browserify = require('browserify')
@@ -17,7 +18,7 @@ var gulp = require('gulp')
   , gutil = require('gulp-util')
   , sourcemaps = require('gulp-sourcemaps')
 
-var paths = {
+const PATHS = {
   jade: ['src/**/*.jade'],
   jadeExcluded: ['*', '!src/include/*.jade'],
   jsBundle: 'src/js/bundle/**/*',
@@ -27,128 +28,139 @@ var paths = {
   static: ['src/img/**/*', 'src/font/**/*'],
 }
 
+// Default html file name, change it if necessary
+// Like '.html'
+const HTML_EXT_NAME = ''
+
+const DEV_SERVER_PORT = '3000'
+
 // Dev related tasks
 
-gulp.task('dev-clean', function() {
+gulp.task('dev-clean', function () {
   return del(['dev/**/*'])
 })
 
-gulp.task('dev-jade', ['dev-clean'], function() {
-  watch(paths.jade)
+gulp.task('dev-jade', ['dev-clean'], function () {
+  watch(PATHS.jade)
     .pipe(affected())
-    .pipe(gulpFilter(paths.jadeExcluded))
+    .pipe(gulpFilter(PATHS.jadeExcluded))
     .pipe(gulpJade({
       pretty: true,
     }))
-    .pipe(rename({extname: '.html'}))
+    .pipe(rename({extname: HTML_EXT_NAME}))
     .pipe(gulp.dest('dev/'))
-  return gulp.src(paths.jade)
-    .pipe(gulpFilter(paths.jadeExcluded))
+  return gulp.src(PATHS.jade)
+    .pipe(gulpFilter(PATHS.jadeExcluded))
     .pipe(gulpJade({
       pretty: true,
     }))
-    .pipe(rename({extname: '.html'}))
+    .pipe(rename({extname: HTML_EXT_NAME}))
     .pipe(gulp.dest('dev/'))
 })
 
 gulp.task('dev-sass', ['dev-clean'], function () {
-  watch(paths.sass)
+  watch(PATHS.sass)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dev/css'))
-  return gulp.src(paths.sass)
+  return gulp.src(PATHS.sass)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dev/css'))
 })
 
 var browserifyOptions = Object.assign({}, watchify.args, {
-  entries: require('glob').sync(paths.jsBundle),
+  entries: require('glob').sync(PATHS.jsBundle),
   debug: true
-});
-var b = watchify(browserify(browserifyOptions));
+})
+var b = watchify(browserify(browserifyOptions))
 b.on('update', bundle)
 b.on('log', gutil.log)
 
-function bundle() {
+function bundle () {
   return b.bundle()
     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
     .pipe(source('bundle.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dev/js'));
+    .pipe(gulp.dest('dev/js'))
 }
 gulp.task('dev-bundle-js', ['dev-clean'], bundle)
 
-gulp.task('dev-js', ['dev-clean'], function() {
-  watch(paths.js)
+gulp.task('dev-js', ['dev-clean'], function () {
+  watch(PATHS.js)
     .pipe(gulp.dest('dev/js'))
-  return gulp.src(paths.js)
+  return gulp.src(PATHS.js)
     .pipe(gulp.dest('dev/js'))
 })
 
 // Build related tasks
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
   return del(['dist'])
 })
 
-gulp.task('copy-static', ['clean'], function() {
-  return gulp.src(paths.static, {base: 'src/'}).pipe(gulp.dest('dist/'))
+gulp.task('copy-static', ['clean'], function () {
+  return gulp.src(PATHS.static, {base: 'src/'}).pipe(gulp.dest('dist/'))
 })
 
 gulp.task('translate-sass', ['clean'], function () {
-  return gulp.src(paths.sass)
+  return gulp.src(PATHS.sass)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dist/css'))
 
 })
 
-gulp.task('compress-css', ['clean', 'translate-sass'], function() {
+gulp.task('compress-css', ['clean', 'translate-sass'], function () {
   return gulp.src('dist/css/**/*.css')
     .pipe(minifyCSS({ keepBreaks: false, keepSpecialComments: 0 }))
     .pipe(gulp.dest('dist/css/'))
 
 })
 
-gulp.task('compress-js', ['clean'], function() {
-  return gulp.src(paths.js)
+gulp.task('compress-js', ['clean'], function () {
+  return gulp.src(PATHS.js)
     .pipe(uglify())
     .pipe(gulp.dest('dist/js/'))
 })
 
-gulp.task('bundle-js', ['clean'], function() {
+gulp.task('bundle-js', ['clean'], function () {
   return browserify(browserifyOptions).bundle()
     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
     .pipe(source('bundle.js'))
     .pipe(buffer())
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/js'));
+    .pipe(gulp.dest('./dist/js'))
 })
 
-gulp.task('translate-jade', ['clean'], function() {
-  return gulp.src(paths.jade)
-    .pipe(gulpFilter(paths.jadeExcluded))
+gulp.task('translate-jade', ['clean'], function () {
+  return gulp.src(PATHS.jade)
+    .pipe(gulpFilter(PATHS.jadeExcluded))
     .pipe(gulpJade())
-    .pipe(rename({extname: '.html'}))
+    .pipe(rename({extname: HTML_EXT_NAME}))
     .pipe(minifyInline())
     .pipe(gulp.dest('dist/'))
 })
 
-gulp.task('dev', ['dev-bundle-js', 'dev-js', 'dev-jade', 'dev-sass'], function() {
+gulp.task('dev', ['dev-bundle-js', 'dev-js', 'dev-jade', 'dev-sass'], function () {
   var finalhandler = require('finalhandler')
     , http = require('http')
     , serveStatic = require('serve-static')
 
-  var serve = serveStatic('dev', {'index': ['index.html', 'index.htm']})
+  var serve = serveStatic('dev', {
+    'index': ['index', 'index.html', 'index.htm'],
+    'setHeaders': function (res, p) {
+      if (path.extname(p) === '') res.setHeader('Content-Type', 'text/html')
+    }
+  })
 
   var server = http.createServer(function onRequest (req, res) {
     serve(req, res, finalhandler(req, res))
   })
 
-  server.listen(3000)
+  server.listen(DEV_SERVER_PORT)
 
-  setTimeout(function() {
-    console.info('Latino is running at http://localhost:3000/. Press Ctrl+C to stop.')
+  setTimeout(function () {
+    console.info(`Latino is running at http://localhost:${DEV_SERVER_PORT}/. Press Ctrl+C to stop.`)
   }, 500)
 })
 
